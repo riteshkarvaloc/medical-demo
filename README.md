c8e51d64a0ffaa6028ec3da9beaded0a65d01fc0
riteshkarvaloc <ritesh.k@oneconvergence.com>
2020-02-14 09:24:21 +0530

    updates

--- README.md
+++ README.md
@@ -9,4 +9,4 @@ python cell-classification/split/annot_split.py
 python cell-classification/model/train_frcnn.py -o simple -p /opt/dkube/input/annot.txt --hf --vf --rot --num_epochs 1
 
 # Evaluation command
-python cell-classification/evaluation/evaluate.py --path /opt/dkube/input/annot.txt
+python cell-classification/model/evaluate.py --path /opt/dkube/input/annot.txt
---
+++ cell-classification/model/evaluate.py
@@ -0,0 +1,247 @@
+from __future__ import division
+import os
+import cv2
+import numpy as np
+import sys
+import pickle
+from optparse import OptionParser
+import time
+import tensorflow as tf
+from keras_frcnn import config
+from tensorflow.keras.optimizers import Adam, SGD, RMSprop
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from keras_frcnn import data_generators
+from keras_frcnn import losses as losses
+import keras_frcnn.roi_helpers as roi_helpers
+from keras_frcnn.simple_parser import get_data
+from keras.utils import generic_utils
+
+def format_img_size(img, C):
+    """ formats the image size based on config """
+    img_min_side = float(C.im_size)
+    (height,width,_) = img.shape
+        
+    if width <= height:
+        ratio = img_min_side/width
+        new_height = int(ratio * height)
+        new_width = int(img_min_side)
+    else:
+        ratio = img_min_side/height
+        new_width = int(ratio * width)
+        new_height = int(img_min_side)
+    img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
+    return img, ratio    
+
+def format_img_channels(img, C):
+    """ formats the image channels based on config """
+    img = img[:, :, (2, 1, 0)]
+    img = img.astype(np.float32)
+    img[:, :, 0] -= C.img_channel_mean[0]
+    img[:, :, 1] -= C.img_channel_mean[1]
+    img[:, :, 2] -= C.img_channel_mean[2]
+    img /= C.img_scaling_factor
+    img = np.transpose(img, (2, 0, 1))
+    img = np.expand_dims(img, axis=0)
+    return img
+
+def format_img(img, C):
+    """ formats an image for model prediction based on config """
+    img, ratio = format_img_size(img, C)
+    img = format_img_channels(img, C)
+    return img, ratio
+
+# Method to transform the coordinates of the bounding box to its original size
+def get_real_coordinates(ratio, x1, y1, x2, y2):
+
+    real_x1 = int(round(x1 // ratio))
+    real_y1 = int(round(y1 // ratio))
+    real_x2 = int(round(x2 // ratio))
+    real_y2 = int(round(y2 // ratio))
+
+    return (real_x1, real_y1, real_x2 ,real_y2)
+
+sys.setrecursionlimit(40000)
+
+parser = OptionParser()
+
+parser.add_option("-p", "--path", dest="test_path", help="Path to test data.")
+parser.add_option("-n", "--num_rois", type="int", dest="num_rois",
+                help="Number of ROIs per iteration. Higher means more memory use.", default=32)
+parser.add_option("--config_filename", dest="config_filename", help=
+                "Location to read the metadata related to the training (generated when training).",
+                default="config.pickle")
+parser.add_option("--network", dest="network", help="Base network to use. Supports vgg or resnet50.", default='resnet50')
+
+(options, args) = parser.parse_args()
+
+if not options.test_path:   # if filename is not given
+    parser.error('Error: path to test data must be specified. Pass --path to command line')
+
+
+data_path = '/opt/dkube/input/'
+
+model_path ='/opt/dkube/model/model/'
+
+config_output_filename = model_path + options.config_filename
+
+with open(config_output_filename, 'rb') as f_in:
+    C = pickle.load(f_in)
+
+if C.network == 'resnet50':
+    import keras_frcnn.resnet as nn
+elif C.network == 'vgg':
+    import keras_frcnn.vgg as nn
+
+# turn off any data augmentation at test time
+C.use_horizontal_flips = False
+C.use_vertical_flips = False
+C.rot_90 = False
+
+class_mapping = C.class_mapping
+    
+    
+all_imgs, classes_count, class_mapping = get_data(options.test_path)
+
+if 'bg' not in classes_count:
+    classes_count['bg'] = 0
+    class_mapping['bg'] = len(class_mapping)
+    
+test_imgs = [s for s in all_imgs if s['imageset'] == 'trainval' or s['imageset'] == 'test']
+
+print('Num train samples {}'.format(len(test_imgs)))
+
+data_gen_test = data_generators.get_anchor_gt(test_imgs, classes_count, C, nn.get_img_output_length, mode='val')
+
+C.num_rois = int(options.num_rois)
+
+if C.network == 'resnet50':
+    num_features = 1024
+elif C.network == 'vgg':
+    num_features = 512
+
+
+input_shape_img = (600,600, 3)
+
+img_input = Input(shape=input_shape_img)
+roi_input = Input(shape=(None, 4))
+# feature_map_input = Input(shape=input_shape_features)
+
+shared_layers = nn.nn_base(img_input, trainable=True)
+
+# define the RPN, built on the base layers
+num_anchors = len(C.anchor_box_scales) * len(C.anchor_box_ratios)
+rpn = nn.rpn(shared_layers, num_anchors)
+
+classifier = nn.classifier(shared_layers, roi_input, C.num_rois, nb_classes=len(classes_count), trainable=True)
+
+model_rpn = Model(img_input, rpn[:2])
+model_classifier = Model([img_input, roi_input], classifier)
+
+C.model_path = model_path + 'weights.h5'
+
+
+print('Loading weights from {}'.format(C.model_path))
+model_rpn.load_weights(C.model_path, by_name=True)
+model_classifier.load_weights(C.model_path, by_name=True)
+
+optimizer = Adam(lr=1e-5)
+optimizer_classifier = Adam(lr=1e-5)
+model_rpn.compile(optimizer=optimizer, loss=[losses.rpn_loss_cls(num_anchors), losses.rpn_loss_regr(num_anchors)])
+model_classifier.compile(optimizer=optimizer_classifier, loss=[losses.class_loss_cls, losses.class_loss_regr(len(classes_count)-1)], metrics={'dense_class_{}'.format(len(classes_count)): 'accuracy'})
+
+################# Evaluating #################################
+start_time = time.time()
+losses = np.zeros((len(test_imgs), 5))
+rpn_accuracy_rpn_monitor = []
+rpn_accuracy_for_epoch = []
+iter_num = 0
+metric_names = ['loss_rpn_cls','loss_rpn_regr', 'loss_class_cls',
+                'loss_class_regr', 'class_acc', 'mean_overlapping_bboxes']
+progbar = generic_utils.Progbar(len(test_imgs))
+print('Evaluating {} samples'.format(len(test_imgs)))
+
+while True:
+    X, Y, img_data = next(data_gen_test)
+    loss_rpn = model_rpn.evaluate(X, Y)
+    P_rpn = model_rpn.predict_on_batch(X)
+
+    R = roi_helpers.rpn_to_roi(P_rpn[0], P_rpn[1], C, use_regr=True, overlap_thresh=0.7, max_boxes=300)
+    # note: calc_iou converts from (x1,y1,x2,y2) to (x,y,w,h) format
+    X2, Y1, Y2, IouS = roi_helpers.calc_iou(R, img_data, C, class_mapping)
+
+    if X2 is None:
+        rpn_accuracy_rpn_monitor.append(0)
+        rpn_accuracy_for_epoch.append(0)
+        continue
+
+    neg_samples = np.where(Y1[0, :, -1] == 1)
+    pos_samples = np.where(Y1[0, :, -1] == 0)
+
+    if len(neg_samples) > 0:
+        neg_samples = neg_samples[0]
+    else:
+        neg_samples = []
+
+    if len(pos_samples) > 0:
+        pos_samples = pos_samples[0]
+    else:
+        pos_samples = []
+    
+    rpn_accuracy_rpn_monitor.append(len(pos_samples))
+    rpn_accuracy_for_epoch.append((len(pos_samples)))
+
+    if C.num_rois > 1:
+        if len(pos_samples) < C.num_rois//2:
+            selected_pos_samples = pos_samples.tolist()
+        else:
+            selected_pos_samples = np.random.choice(pos_samples, C.num_rois//2, replace=False).tolist()
+        try:
+            selected_neg_samples = np.random.choice(neg_samples, C.num_rois - len(selected_pos_samples), replace=False).tolist()
+        except:
+            selected_neg_samples = np.random.choice(neg_samples, C.num_rois - len(selected_pos_samples), replace=True).tolist()
+
+        sel_samples = selected_pos_samples + selected_neg_samples
+    else:
+        # in the extreme case where num_rois = 1, we pick a random pos or neg sample
+        selected_pos_samples = pos_samples.tolist()
+        selected_neg_samples = neg_samples.tolist()
+        if np.random.randint(0, 2):
+            sel_samples = random.choice(neg_samples)
+        else:
+            sel_samples = random.choice(pos_samples)
+
+    loss_class = model_classifier.train_on_batch([X, X2[:, sel_samples, :]], [Y1[:, sel_samples, :], Y2[:, sel_samples, :]])
+
+    losses[iter_num, 0] = loss_rpn[1]
+    losses[iter_num, 1] = loss_rpn[2]
+
+    losses[iter_num, 2] = loss_class[1]
+    losses[iter_num, 3] = loss_class[2]
+    losses[iter_num, 4] = loss_class[3]
+
+    progbar.update(iter_num+1, [('rpn_cls', losses[iter_num, 0]), ('rpn_regr', losses[iter_num, 1]),
+                                ('detector_cls', losses[iter_num, 2]), ('detector_regr', losses[iter_num, 3])])
+
+    iter_num += 1
+    
+    if iter_num == len(test_imgs):
+        loss_rpn_cls = np.mean(losses[:, 0])
+        loss_rpn_regr = np.mean(losses[:, 1])
+        loss_class_cls = np.mean(losses[:, 2])
+        loss_class_regr = np.mean(losses[:, 3])
+        class_acc = np.mean(losses[:, 4])
+        mean_overlapping_bboxes = float(sum(rpn_accuracy_for_epoch)) / len(rpn_accuracy_for_epoch)
+        
+        rpn_accuracy_for_epoch = []
+        
+        if C.verbose:
+            print('Mean number of bounding boxes from RPN overlapping ground truth boxes: {}'.format(mean_overlapping_bboxes))
+            print('Classifier accuracy for bounding boxes from RPN: {}'.format(class_acc))
+            print('Loss RPN classifier: {}'.format(loss_rpn_cls))
+            print('Loss RPN regression: {}'.format(loss_rpn_regr))
+            print('Loss Detector classifier: {}'.format(loss_class_cls))
+            print('Loss Detector regression: {}'.format(loss_class_regr))
+            print('Elapsed time: {}'.format(time.time() - start_time))
+        break
